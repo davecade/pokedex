f3045820c9fdf4f76554b51f43f9ae2fe2217e12
Dave <dave.cadelina@outlook.com>
2020-12-23 03:19:01 +1100

    added pokemon API comments for future reference

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,16 @@
 import logo from '../logo.svg';
 import '../css/main.css';
 
+// -- Pokemon Image API: https://pokeres.bastionbot.org/images/pokemon/1.png
+// -- Pokemon Data API: https://pokeapi.co/api/v2/pokemon/1
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pokedex</h1>
       </header>
-    </div>
+    </div>git 
   );
 }
 
