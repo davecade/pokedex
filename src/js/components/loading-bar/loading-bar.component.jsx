c3a43f32d21e6f2473ae27e013df1fa26e84e634
Dave <dave.cadelina@outlook.com>
2020-12-26 22:10:58 +1100

    removed the if statement checking length in the loading-bar component

--- src/js/components/loading-bar/loading-bar.component.jsx
+++ src/js/components/loading-bar/loading-bar.component.jsx
@@ -1,25 +1,23 @@
 import React, { Fragment } from 'react';
 
 export const LoadingBar = ({length}) => {
+    let percent;
 
     // -- Runs loading bar if loading is in progress
     const loadingInProgress = () => {
         const LoadngBarEl = document.querySelector('.loading-bar')
         if(LoadngBarEl) {
             let currentLength = length
-            let percent = Math.floor((currentLength/151)*100)
+            percent = Math.floor((currentLength/151)*100)
             LoadngBarEl.style = `width: ${percent}%`
         }
     }
-
-    if(length<151) {
-        loadingInProgress()
-    }
+    loadingInProgress()
 
     return (
         <Fragment>
             <h1>Loading Pokemon</h1>
-            <h1>{length}</h1>
+            <h1>{percent}%</h1>
             <div className="loading-container">
                 <div className="loading-bar"></div>
             </div>
