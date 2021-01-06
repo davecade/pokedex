be9c48f7a820c712d275973b873087ed6586d734
Dave <dave.cadelina@outlook.com>
2021-01-06 22:31:04 +1100

    added fixed header

--- src/js/components/display/display.component.jsx
+++ src/js/components/display/display.component.jsx
@@ -1,15 +1,22 @@
 import React, { Fragment } from 'react';
 import { SearchBar } from '../search-bar/search-bar.component'
 import { LoadingBar } from '../loading-bar/loading-bar.component'
+import './display.styles.scss'
 
 export const Display = ({length, title, handleChange}) => {
   if(length < 151) {
-      return <LoadingBar length={length}/>
+      return (
+        <div className="display">
+            <LoadingBar length={length}/>
+        </div>
+      )
     } else {
       return (
         <Fragment>
-            <h1 className="title">{title}</h1>
-            <SearchBar handleChange={handleChange} />
+            <div className="display">
+                <h1 className="title">{title}</h1>
+                <SearchBar handleChange={handleChange} />
+            </div>
         </Fragment>
       )
     }
