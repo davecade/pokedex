26989dd249e1fdddf15ad93e417e6b4a7c6ec995
Dave <dave.cadelina@outlook.com>
2021-01-23 00:13:10 +1100

    added positioning for evolution tree

--- src/js/components/cardview/cardview.component.jsx
+++ src/js/components/cardview/cardview.component.jsx
@@ -27,27 +27,8 @@ export const CardView = ({pokemon, handleClickOnCard, pokemonList}) => {
                 <h1 className="cardview-name">{pokemon.name}</h1>
             </div> 
             
-            <div className="cardview-content">  
-                <div className="cardview-stats">
-                    <div className="stat-title">
-                        <p>TYPE</p>
-                        <p>HP</p>
-                        <p>ATTACK / DEFENSE</p>
-                        <p>SPEED</p>
-                        <p>HEIGHT</p>
-                        <p>WEIGHT</p>
-                    </div>
-                    <div className="stat-value">
-                        <p>{pokemon.type[0] ? pokemon.type[0].toUpperCase() : ''} {pokemon.type[1] ? `and ${pokemon.type[1].toUpperCase()}` : ''}</p>
-                        <p>{pokemon.hp}</p>
-                        <p>{pokemon.attack} / {pokemon.defense}</p>
-                        <p>{pokemon.speed}</p>
-                        <p>{pokemon.height/10} m</p>
-                        <p>{pokemon.weight/10} kg</p>
-                    </div>
-                </div>
-
-                <div className="cardview-pokemon">
+            <div className="cardview-content">
+                <div className="left">
                     <div className="evolution-tree">
                         {
                             evolveTree.map(id => {
@@ -56,6 +37,28 @@ export const CardView = ({pokemon, handleClickOnCard, pokemonList}) => {
                             
                         }
                     </div>
+                    <div className="cardview-stats">
+                        <div className="stat-title">
+                            <p>TYPE</p>
+                            <p>HP</p>
+                            <p>ATTACK / DEFENSE</p>
+                            <p>SPEED</p>
+                            <p>HEIGHT</p>
+                            <p>WEIGHT</p>
+                        </div>
+                        <div className="stat-value">
+                            <p>{pokemon.type[0] ? pokemon.type[0].toUpperCase() : ''} {pokemon.type[1] ? `and ${pokemon.type[1].toUpperCase()}` : ''}</p>
+                            <p>{pokemon.hp}</p>
+                            <p>{pokemon.attack} / {pokemon.defense}</p>
+                            <p>{pokemon.speed}</p>
+                            <p>{pokemon.height/10} m</p>
+                            <p>{pokemon.weight/10} kg</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="cardview-pokemon">
                     <img src={pokemon.image} alt="" className="cardview-img"/>
                 </div>
                 
