7975c9c3be4932198b400a53863f76ec7c65e6f5
Dave <dave.cadelina@outlook.com>
2021-03-22 00:14:09 +1100

    added styling to the radio buttons

--- src/js/components/type-selector/type-selector.component.jsx
+++ src/js/components/type-selector/type-selector.component.jsx
@@ -4,12 +4,12 @@ import './type-selector.styles.scss'
 export const TypeSelector = () => {
     return(
         <div class="typeSelector">
-            <input type="radio" id="Fire" name="type" value="Fire" />
-            <label for="male">Fire</label>
-            <input type="radio" id="Grass" name="type" value="Grass" />
-            <label for="female">Grass</label>
-            <input type="radio" id="Water" name="type" value="Water" />
-            <label for="other">Water</label>
+            <input type="radio" class="radio" id="Fire" name="type" value="Fire" />
+            <label for="male" class="label">Fire</label>
+            <input type="radio" class="radio" id="Grass" name="type" value="Grass" />
+            <label for="female" class="label">Grass</label>
+            <input type="radio" class="radio" id="Water" name="type" value="Water" />
+            <label for="other" class="label">Water</label>
         </div>
     )
 }
