6d718ba4ef9972d6df8797b9ab3894a3d9131c5a
Dave <dave.cadelina@outlook.com>
2021-03-22 00:04:34 +1100

    added test labels and radio button for tthe tpe selectio

--- src/js/components/type-selector/type-selector.component.jsx
+++ src/js/components/type-selector/type-selector.component.jsx
@@ -4,7 +4,12 @@ import './type-selector.styles.scss'
 export const TypeSelector = () => {
     return(
         <div class="typeSelector">
-            <p> Type Selector </p>
+            <input type="radio" id="Fire" name="type" value="Fire" />
+            <label for="male">Fire</label>
+            <input type="radio" id="Grass" name="type" value="Grass" />
+            <label for="female">Grass</label>
+            <input type="radio" id="Water" name="type" value="Water" />
+            <label for="other">Water</label>
         </div>
     )
 }
