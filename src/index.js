197f7b6e518ba3333768acf72664b92a28e498d2
Dave <dave.cadelina@outlook.com>
2021-05-16 14:37:56 +1000

    installed redux and also wrapped Provider around App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App.js';
 import './index.styles.scss';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
