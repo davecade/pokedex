1d477c25ddac9bc528ee6de7d41493dbffa32b4e
Dave <dave.cadelina@outlook.com>
2021-09-13 01:34:34 +1000

    updated comments

--- src/js/redux/pokemon/pokemon.sagas.js
+++ src/js/redux/pokemon/pokemon.sagas.js
@@ -21,7 +21,6 @@ const removeUnwantedArrowText = text => {
 export function* selectPokemonAsync({payload}) {
 
     // -- Pokemon Image API: https://pokeres.bastionbot.org/images/pokemon/1.png
-    // -- image: `./images/pokemon_${i}.jpg`,
     // -- Pokemon Data API: https://pokeapi.co/api/v2/pokemon/1
 
     try {
