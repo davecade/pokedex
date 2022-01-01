b31a445ee7aa3d1b6ce212bddd27aebff45326ec
Dave <dave.cadelina@outlook.com>
2022-01-02 09:33:52 +1100

    applied prettier to code

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './js/App.js';
-import './index.styles.scss';
-import { Provider } from 'react-redux'
-import store from './js/redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./js/App.js";
+import "./index.styles.scss";
+import { Provider } from "react-redux";
+import store from "./js/redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
--- client/src/js/redux/pokemon/pokemon.sagas.js
+++ client/src/js/redux/pokemon/pokemon.sagas.js
@@ -1,36 +1,30 @@
-import { takeLatest, put, all, call, } from "redux-saga/effects";
+import { takeLatest, put, all, call } from "redux-saga/effects";
 import { PokemonActionTypes } from "./pokemon.types";
-import { enableModal } from '../modal/modal.actions'
+import { enableModal } from "../modal/modal.actions";
 import axios from "axios";
 import {
     fetchPokemonDataSuccess,
     fetchPokemonDataFailure,
     addPokemonToSelected,
-    startLoading
+    startLoading,
 } from "./pokemon.actions";
 
-
-export function* selectPokemonAsync({payload}) {
-
+export function* selectPokemonAsync({ payload }) {
     try {
-            yield put(startLoading())
-            const pokeData = yield axios.get(`/pokemon/${payload}`)
-            yield put(addPokemonToSelected(pokeData.data))
-            yield put(fetchPokemonDataSuccess())
-            yield put(enableModal())
-
-    } catch(error) {
-        yield put(fetchPokemonDataFailure(error))
+        yield put(startLoading());
+        const pokeData = yield axios.get(`/pokemon/${payload}`);
+        yield put(addPokemonToSelected(pokeData.data));
+        yield put(fetchPokemonDataSuccess());
+        yield put(enableModal());
+    } catch (error) {
+        yield put(fetchPokemonDataFailure(error));
     }
 }
 
-
-export function* onSelectPokemon(){
-    yield takeLatest(PokemonActionTypes.SELECT_POKEMON, selectPokemonAsync)
+export function* onSelectPokemon() {
+    yield takeLatest(PokemonActionTypes.SELECT_POKEMON, selectPokemonAsync);
 }
 
 export function* pokemonSagas() {
-    yield all([
-        call(onSelectPokemon)
-    ])
+    yield all([call(onSelectPokemon)]);
 }
